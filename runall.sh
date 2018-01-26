c18621aabc7a7d173e89b678acc0a62f257de100
Benjamin Bengfort <benjamin@bengfort.com>
2018-01-26 17:04:24 -0500

    run all with leave 2 out

--- runall.sh
+++ runall.sh
@@ -5,4 +5,6 @@ YPOS="recovery_time_mins_pos"
 YNEG="recovery_time_mins_neg"
 EXCLUDE="building_id,date"
 
-python3 loran.py -n 0 -e $EXCLUDE -o fixtures/esb-model-ranking.json -t $Y fixtures/esb-dataset.csv
+python3 loran.py -n 2 -e $EXCLUDE -o fixtures/esb_model_ranking.json -t $YPOS fixtures/esb-dataset-pos.csv
+python3 loran.py -n 2 -e $EXCLUDE -o fixtures/esb_model_ranking.json -t $YNEG fixtures/esb-dataset-neg.csv
+python3 loran.py -n 2 -e $EXCLUDE -o fixtures/esb_model_ranking.json -t $Y fixtures/esb-dataset.csv
